07f6b1cfc231c766351b5690e4d4d5203ed53251
Justine Ayroor <justineayroor@Justines-MacBook-Pro.local>
2018-10-03 02:13:01 -0400

    Exception Handling Done

--- NW_1/DNS.java
+++ NW_1/DNS.java
@@ -44,54 +44,65 @@ public class DNS
 		}
 		return trimByteArray;
 	}
+	static int portNumber;
 	public static void main(String[] args) throws IOException {
 		if(args.length != 1) {
 			System.err.println("Usage: java DNS <port number>");
 			System.exit(1);
 		}
-		int portNumber = Integer.parseInt(args[0]);
-		System.out.println("DNS Server listening on socket " + portNumber);
-		int servingRequest = 0;
-		ServerSocket ss = null;
 		try {
+			portNumber = Integer.parseInt(args[0]);
+			System.out.println("DNS Server listening on socket " + portNumber);
+			int servingRequest = 0;
+			ServerSocket ss = null;
+		// try {
 			ss = new ServerSocket();
 			ss.bind(new InetSocketAddress(portNumber));
-		} catch (IOException e) {
+		// } catch (IOException e) {
+		// 	System.out.println("Some error occured while listening on port " + portNumber);
+		// 	System.out.println("[P01 DNS - Error]: "+e.getMessage());
+		// 	System.exit(1);
+		// }
+			while (true) {
+				Socket cs = null;
+				OutputStream out = null;
+				InputStream in = null;
+				// try {
+					cs = ss.accept();
+					out = cs.getOutputStream();
+					in = cs.getInputStream();
+					System.out.println("(" + ++servingRequest + ") Incoming client connection from [" + cs.getInetAddress().getHostAddress() + ":" + cs.getPort() + "] to me [" + ss.getInetAddress().getHostAddress() + ":" + ss.getLocalPort() + "]");
+					byte[] requestedHost = new byte[100];
+					in.read(requestedHost);
+					requestedHost = trimBytes(requestedHost);
+					System.out.println("\tREQ: " + new String(requestedHost));
+					//char[] host_url = ConvByteToChar(requestedHost);
+					InetAddress[] ip_list = dns(ConvByteToChar(requestedHost));
+					for(InetAddress host:ip_list)
+					{
+						byte[] ip = host.getHostAddress().getBytes();
+						out.write('I');
+						out.write('P');
+						out.write(':');
+						out.write(ip);
+						out.write('\n');
+					}
+					byte[] output = "Preferred IP: ".getBytes();
+					Socket req_host = new Socket(new String(ConvByteToChar(requestedHost)), 80);
+					byte[] pref_ip = req_host.getInetAddress().getHostAddress().getBytes();
+					out.write(output);
+					out.write(pref_ip);
+					out.write('\n');
+					cs.close();
+			} 
+		}catch (IOException e) {
 			System.out.println("Some error occured while listening on port " + portNumber);
 			System.out.println("[P01 DNS - Error]: "+e.getMessage());
-		}
-		while (true) {
-			Socket cs = null;
-			OutputStream out = null;
-			InputStream in = null;
-			try {
-				cs = ss.accept();
-				out = cs.getOutputStream();
-				in = cs.getInputStream();
-				System.out.println("(" + ++servingRequest + ") Incoming client connection from [" + cs.getInetAddress().getHostAddress() + ":" + cs.getPort() + "] to me [" + ss.getInetAddress().getHostAddress() + ":" + ss.getLocalPort() + "]");
-				byte[] requestedHost = new byte[100];
-				in.read(requestedHost);
-				requestedHost = trimBytes(requestedHost);
-				System.out.println("\tREQ: " + new String(requestedHost));
-				//char[] host_url = ConvByteToChar(requestedHost);
-				InetAddress[] ip_list = dns(ConvByteToChar(requestedHost));
-				for(InetAddress host:ip_list)
-				{
-					byte[] ip = host.getHostAddress().getBytes();
-					out.write(ip);
-					out.write('\n');
-				}
-				byte[] output = "Preferred IP: ".getBytes();
-				Socket req_host = new Socket(new String(ConvByteToChar(requestedHost)), 80);
-				byte[] pref_ip = req_host.getInetAddress().getHostAddress().getBytes();
-				out.write(output);
-				out.write(pref_ip);
-				out.write('\n');
-				cs.close();
-			} catch (IOException e) {
-				System.out.println("Some error occured while listening on port " + portNumber);
-				System.out.println("[P01 DNS - Error]: "+e.getMessage());
-			}
+			System.exit(1);
+		} catch (NumberFormatException e){
+			System.out.println("Usage: java DNS <port number> should be in Integer Format");
+			System.out.println("[P01 DNS - Error]: "+e.getMessage());
+			System.exit(1);
 		}
 	}
 }
