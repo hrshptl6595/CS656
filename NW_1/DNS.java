a362b9f3c2b50d76f6ce9fb8e82f152619eb509f
Justine Ayroor <justineayroor@justines-mbp.resource.campus.njit.edu>
2018-10-05 19:48:21 -0400

    Added Comments

--- NW_1/DNS.java
+++ NW_1/DNS.java
@@ -1,3 +1,4 @@
+
 /*
  * CS656-003 Group M6
  * Harsh Patel(hsp52), Justin Ayoor(ja573), Vishal Singh(vas27),
@@ -14,96 +15,106 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
 
-public class DNS 
+public class DNS
 {
-	public static char[] ConvByteToChar(byte[] byteArray)
-	{
-		char[] charArray = (new String(byteArray)).toCharArray();
-		return charArray;
-	}
-	public static int ConvCharToInt(char[] charArray)
+	public static int ConvCharToInt(char[] charArray) // Used to parse Port Number into Int Format
 	{
 		int num = 0;
-		for(int i=0; i<charArray.length; i++)
-		{
+		for(int i=0; i<charArray.length; i++) {
 			num = num*10 + (charArray[i] - '0');
 		}
 		return num;
 	}
-	public static InetAddress[] dns(char[] url) throws UnknownHostException
-	{
-		InetAddress[] ip_list = InetAddress.getAllByName(new String(url));
-		return ip_list;
-	}
-	public static byte[] trimBytes(byte[] byteArr)
+
+	public static byte[] trimBytes(byte[] byteArr) // Used to trim the byte array to get correct array size
 	{
 		int newlen = 0;
-		for(int i=0;i<=byteArr.length;i++)
-		{
-			if(byteArr[i]==13)
-			{
+		for(int i=0;i<=byteArr.length;i++) {
+			if(byteArr[i] == 13) {		// 13 is the ASCII value for Enter
 				newlen = i;
 				break;
 			}
 		}
 		byte[] trimByteArray = new byte[newlen];
-		for(int i=0;i<trimByteArray.length;i++)
-		{
+		for(int i=0;i<trimByteArray.length;i++) {
 			trimByteArray[i] = byteArr[i];
 		}
 		return trimByteArray;
 	}
+
+	// Used to Calculate Round trip time of one IP Address from the IP List
+	public static int rtt(byte[] req_ip) throws UnknownHostException
+	{
+		long start_time = System.nanoTime();
+		try{
+			InetAddress obj = InetAddress.getByAddress(req_ip);
+			long finish_time = System.nanoTime();
+			return (int)(finish_time - start_time);
+		}catch (UnknownHostException e)
+		{
+			return 0;
+		}		
+
+	}
+
+	public static void dns(byte[] host, OutputStream out) // Main DNS method
+	{
+		try {
+			int min_time = 0; 
+			InetAddress preferredIP = null; 
+			try {
+				InetAddress[] ip_list = InetAddress.getAllByName(new String(host)); // Get list of IP Addresses
+				for(InetAddress hostIP:ip_list) { // Iteration throughout the IP List
+					out.write((" IP: " + hostIP.getHostAddress() + "\n").getBytes()); // Send IP List to Client
+					int roundTT = rtt(hostIP.getAddress()); // Calculate Round Trip Time
+					if(min_time == 0 || min_time>roundTT) // If Rtt of IP is less than the previous IP Rtt
+					{
+						min_time = roundTT;
+						preferredIP = hostIP; // Update the Preferred IP
+					}
+				}
+				out.write(" Preferred IP: ".getBytes()); 
+				out.write((preferredIP.getHostAddress() + "\n").getBytes()); // Send Preferred IP to Client
+			} catch (UnknownHostException e) {
+				out.write(("NO IP ADDRESS FOUND\n").getBytes());
+			}
+		} catch (IOException | NumberFormatException e) {
+			System.out.println("Some error occured while listening on port " + portNumber);
+			System.out.println("[P01 DNS - Error]: "+e.getMessage());
+			System.exit(1);
+		}
+		return;
+	}
+
 	static int portNumber;
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws IOException
+	{
 		if(args.length != 1) {
 			System.err.println("Usage: java DNS <port number>");
 			System.exit(1);
 		}
 		try {
-			char[] p_num = args[0].toCharArray();
-			//System.out.print(p_num);
-			portNumber = ConvCharToInt(p_num);
+			char[] p_num = args[0].toCharArray(); // Converting the String Format into a Char array 
+			portNumber = ConvCharToInt(p_num); // Conver the Char array to get the Port Number in Integer Format
 			System.out.println("DNS Server listening on socket " + portNumber);
 			int servingRequest = 0;
-			ServerSocket ss = null;
-			ss = new ServerSocket();
-			ss.bind(new InetSocketAddress(portNumber));
-			while (true) {
-				Socket cs = null;
-				OutputStream out = null;
-				InputStream in = null;
-					cs = ss.accept();
-					out = cs.getOutputStream();
-					in = cs.getInputStream();
-					System.out.println("(" + ++servingRequest + ") Incoming client connection from [" + cs.getInetAddress().getHostAddress() + ":" + cs.getPort() + "] to me [" + ss.getInetAddress().getHostAddress() + ":" + ss.getLocalPort() + "]");
-					byte[] requestedHost = new byte[100];
-					in.read(requestedHost);
-					requestedHost = trimBytes(requestedHost);
-					System.out.println("\tREQ: " + new String(requestedHost));
-					InetAddress[] ip_list = dns(ConvByteToChar(requestedHost));
-					for(InetAddress host:ip_list)
-					{
-						byte[] ip = host.getHostAddress().getBytes();
-						out.write('I');
-						out.write('P');
-						out.write(':');
-						out.write(ip);
-						out.write('\n');
-					}
-					byte[] output = "Preferred IP: ".getBytes();
-					Socket req_host = new Socket(new String(ConvByteToChar(requestedHost)), 80);
-					byte[] pref_ip = req_host.getInetAddress().getHostAddress().getBytes();
-					out.write(output);
-					out.write(pref_ip);
-					out.write('\n');
-					cs.close();
-			} 
-		}catch (IOException e) {
+			ServerSocket server = new ServerSocket(); // Make a Server Socket
+			server.bind(new InetSocketAddress(portNumber)); // Bind Socket with Port Number
+			while (true) { // Printing Server Side Information
+				Socket client = server.accept(); // Make a Server Socket
+				InputStream client_in = client.getInputStream(); 
+				OutputStream client_out = client.getOutputStream();
+				System.out.println("(" + ++servingRequest + ") Incoming client connection from [" + client.getInetAddress().getHostAddress() + ":" + client.getPort() + "] to me [" + InetAddress.getLocalHost().getHostAddress() + ":" + server.getLocalPort() + "]");
+				byte[] requestedHost = new byte[1024]; // Creating byte array for Host Name
+				client_in.read(requestedHost, 0, requestedHost.length); //
+				requestedHost = trimBytes(requestedHost);
+				System.out.println("\tREQ: " + new String(requestedHost));
+				dns(requestedHost, client_out);
+				client.close();
+			}
+		}catch (IOException | NumberFormatException e) {
 			System.out.println("Some error occured while listening on port " + portNumber);
-			System.out.println("[P01 DNS - Error]: "+e.getMessage());
-			System.exit(1);
-		} catch (NumberFormatException e){
-			System.out.println("Usage: java DNS <port number> should be in Integer Format");
 			System.out.println("[P01 DNS - Error]: "+e.getMessage());
 			System.exit(1);
 		}
