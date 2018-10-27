2921cbf6faf357621f7f1f0b941fedf0deb25d3d
Harsh Patel <harshsatish.patel3@gmail.com>
2018-10-27 17:59:54 +0000

    NW2 initial commit

---
+++ NW2/Web.java
@@ -0,0 +1,15 @@
+/*
+ * CS656-003 Group M6
+ * Harsh Patel(hsp52), Justin Ayoor(ja573), Vishal Singh(vas27),
+ * Rajeev Chanchlani(rnc26), Meghashyam Senapthi(ms2727), Gaurav Daxini(gnd6)
+ */
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
